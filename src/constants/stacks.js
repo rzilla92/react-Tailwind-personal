d2306fa817995ceb132e73530654ea7d6ccb3f9f
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-12-13 11:17:45 +0700

    Update stacks.js

--- src/constants/stacks.js
+++ src/constants/stacks.js
@@ -3,6 +3,8 @@ const stacks = [
     badge: "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"},
     {name: "React Native",
     badge: "https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"},
+    {name: "TypeScript",
+    badge: "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"},
     {name: "TailwindCSS",
     badge: "https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"},
     {name: "Vite",
